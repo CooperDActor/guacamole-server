9fd54b27cb37b0cda415dad9b0e043e37e949b4a
Cooper D'Andilly <151992386+CooperDActor-bytes@users.noreply.github.com>
2024-04-23 16:23:53 +1000

    Update Depend-on-debian.sh

--- Depend-on-debian.sh
+++ Depend-on-debian.sh
@@ -1,3 +1,59 @@
+#!/bin/bash
+
+identify_and_run() {
+    os=$(uname)
+    if [ "$os" = "Linux" ]; then
+        distro=$(lsb_release -si)
+        case "$distro" in
+            CentOS|RedHatEnterpriseServer)
+                sudo yum install <your-package-name>
+                ;;
+            Ubuntu|Debian)
+                # FFmpeg
+sudo apt-get install -y libavcodec-dev libavformat-dev libavutil-dev libswscale-dev
+
+# FreeRDP
+sudo apt-get install -y freerdp2-dev
+
+# Pango
+sudo apt-get install -y libpango1.0-dev
+
+# libssh2
+sudo apt-get install -y libssh2-1-dev
+
+# libtelnet
+sudo apt-get install -y libtelnet-dev
+
+# libVNCServer
+sudo apt-get install -y libvncserver-dev
+
+# libwebsockets
+sudo apt-get install -y libwebsockets-dev
+
+# PulseAudio
+sudo apt-get install -y libpulse-dev
+
+# OpenSSL
+sudo apt-get install -y libssl-dev
+
+# libvorbis
+sudo apt-get install -y libvorbis-dev
+
+# libwebp
+sudo apt-get install -y libwebp-dev
+
+                ;;
+            *)
+                echo "Unsupported Linux distribution."
+                ;;
+        esac
+    else
+        echo "Unsupported operating system."
+    fi
+}
+
+identify_and_run
+
 # FFmpeg
 sudo apt-get install -y libavcodec-dev libavformat-dev libavutil-dev libswscale-dev
 
